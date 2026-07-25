edb1fde6b5221c6978403b6fc7dc3fbccd27ecc9
Awadhesh Kumar <kumaravi0506@gmail.com>
2026-07-26 02:02:40 +0530

    added new licence input form component

--- vite-project/src/components/new_licence.jsx
+++ vite-project/src/components/new_licence.jsx
@@ -10,27 +10,27 @@ const NewDrivingLicence = () => {
                 <div className="form-container">
                     <div className="front-container">
                         <div>
-                            <h4>Driving Licence :</h4>
+                            <h4>Driving Licence Number :</h4>
                             <input type="text" placeholder="Enter Driving Licence Number" />
                         </div>
                         <div>
-                            <h4>Issue Date :</h4>
+                            <h4>Licence Issue Date :</h4>
                             <input type="date" placeholder="Select issue date"/>
                         </div>
                         <div>
-                            <h4>validity :</h4>
+                            <h4> Licence Validity :</h4>
                             <input type="date" placeholder="Select Validity date" />
                         </div>
                         <div>
-                            <h4>Photo :</h4>
+                            <h4>Holder's Photo :</h4>
                             <input type="file" placeholder="Upload photo" />
                         </div>
                         <div>
-                            <h4>Signature :</h4>
+                            <h4>Holder's Signature :</h4>
                             <input type="file" placeholder="Upload signature" />
                         </div>
                         <div>
-                            <h4>Name :</h4>
+                            <h4>Holder's Name :</h4>
                             <input type="text" placeholder="Enter your name" />
                         </div>
                         <div>
@@ -38,17 +38,29 @@ const NewDrivingLicence = () => {
                             <input type="date" placeholder="Select your date of birth" />
                         </div>
                         <div>
-                            <h4>Father's Name</h4>
+                            <h4>Father's Name :</h4>
                             <input type="text" placeholder="Enter your father's name" />
                         </div>
                         <div>
                             <h4>Address :</h4>
-                            <textarea name="" id=""></textarea>
+                            <textarea name="" id="" placeholder="Enter your address"></textarea>
                         </div>
                     </div>
                     <div className="back-container">
-
+                        <div>
+                            <h4>Vehicle Code :</h4>
+                            <input type="text" placeholder="Enter vehicle code" />
+                        </div>
+                        <div>
+                            <h4>Mobile Number :</h4>
+                            <input type="number" placeholder="Enter your mobile number" />
+                        </div>
+                        <div>
+                            <h4>Licence Authority :</h4>
+                            <input type="text" placeholder="Enter licence authority name" />
+                        </div>
                     </div>
+                    <div className="submit-btn-container"><button>Submit</button></div>
                 </div>
             </main>
         </>
